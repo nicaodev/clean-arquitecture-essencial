39df87de99b5248ae76e948b55ae1cf66de05690
nicaodev <>
2023-01-30 20:13:01 -0300

    Implementação concreta CategoryRepository

--- CleanArchMvc/CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -10,7 +10,7 @@ namespace CleanArchMvc.Domain.Interfaces
     public interface ICategoryRepository
     {
         Task<IEnumerable<Category>> GetCategoriesAsync();
-        Task<IEnumerable<Category>> GetByIdAsync(int? id);
+        Task<Category> GetByIdAsync(int? id);
         Task<Category> CreateAsync(Category category);
         Task<Category> UpdateAsync(Category category);
         Task<Category> RemoveAsync(Category category);
---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(i => i.Id);
+            builder.Property(n => n.Name).HasMaxLength(100).IsRequired();
+            builder.Property(d => d.Description).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Price).HasPrecision(10, 2);
+
+            builder.HasOne(e => e.Category).WithMany(e => e.Products).HasForeignKey(e => e.CategoryId);
+
+        }
+    }
+}
