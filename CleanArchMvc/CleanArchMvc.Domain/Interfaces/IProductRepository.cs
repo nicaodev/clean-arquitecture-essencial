7253114fe787895251537021634bd04d41f52895
nicaodev <>
2023-01-31 21:59:53 -0300

    ProductRepository

--- CleanArchMvc/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -11,8 +11,8 @@ namespace CleanArchMvc.Domain.Interfaces
     {
 
         Task<IEnumerable<Product>> GetProductsAsync();
-        Task<IEnumerable<Product>> GetByIdAsync(int? id);
-        Task<IEnumerable<Product>> GetProductCategoryAsync(int? id);
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
         Task<Product> CreateAsync(Product product);
         Task<Product> UpdateAsync(Product product);
         Task<Product> RemoveAsync(Product product);
