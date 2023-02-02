998fb61f6095f44a8d3fbdb171828bbd427c523d
nicaodev <>
2023-02-01 23:26:16 -0300

    include migrations

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Migrations/20230202021938_seedProducts.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    public partial class seedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" +
+                "VALUES('Caderno espiral', 'Caderno 100 folhas', 7.45, 50, 'caderno1.png',1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" +
+                "VALUES('Estojo', 'Estojo Preto', 1.45, 23, 'estojo1.png',1)");
+
+            migrationBuilder.Sql("INSERT INTO Products(Name, Description, Price, Stock, Image, CategoryId)" +
+                "VALUES('Lapis', 'Lapis Azul', 2.45, 50, 'Lapiz.png',1)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
