e7f6bedf79449d3eed350a904c1f58d2bef228b6
nicaodev <alexandre.nicos@gmail.com>
2023-02-09 22:45:04 -0300

    .

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Identity/SeedUserRoleInitial.cs
@@ -0,0 +1,31 @@
+﻿using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Identity
+{
+    public class SeedUserRoleInitial : ISeedUserRoleInitial
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public SeedUserRoleInitial(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public void SeedRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SeedUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
