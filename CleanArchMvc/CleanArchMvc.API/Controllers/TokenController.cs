3df48bba3d3497db9573fbba05bddb8cbf5498a2
nicaodev <alexandre.nicos@gmail.com>
2023-02-14 20:30:01 -0300

    without GenerateToken

---
+++ CleanArchMvc/CleanArchMvc.API/Controllers/TokenController.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchMvc.API.Models;
+using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticate _authenticate;
+
+        public TokenController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate ?? throw new ArgumentNullException(nameof(authenticate));
+        }
+
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginModel loginModel)
+        {
+            var result = await _authenticate.Authenticate(loginModel.Email, loginModel.Password);
+
+            if (result)
+            {
+                //return GenerateToken(loginModel);
+                return Ok("user login successfully");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
